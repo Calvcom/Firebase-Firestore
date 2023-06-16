060229b815f90a5c0cd037f831b721283f56a541
blas <biasserico@gmail.com>
2023-06-16 07:50:27 -0500

    Creacion paginas y componentes

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { GraficoBarraHorizontalComponent } from './grafico-barra-horizontal/grafico-barra-horizontal.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    GraficoBarraHorizontalComponent
+  ],
+  exports: [
+    NavbarComponent,
+    GraficoBarraHorizontalComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-barra-horizontal',
+  templateUrl: './grafico-barra-horizontal.component.html',
+  styleUrls: ['./grafico-barra-horizontal.component.css']
+})
+export class GraficoBarraHorizontalComponent {
+
+}
